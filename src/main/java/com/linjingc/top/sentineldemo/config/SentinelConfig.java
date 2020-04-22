3677b3089c0fa8b02d6b70fb20522212e82be7fe
chenxiaocehng <583188551@qq.com>
2020-04-22 09:35:28 +0800

    提交 SphU 和SphO的用法

--- src/main/java/com/linjingc/top/sentineldemo/config/SentinelConfig.java
+++ src/main/java/com/linjingc/top/sentineldemo/config/SentinelConfig.java
@@ -3,14 +3,15 @@ package com.linjingc.top.sentineldemo.config;
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Sentinel 配置
+ *
  * @date 2020年4月21日17:24:54
  */
 @Configuration
@@ -18,9 +19,8 @@ public class SentinelConfig {
 	/**
 	 * 需要配置执行日志位置 不然会在当前用户下 的logs/csp/文件夹下生成
 	 * spring.cloud.sentinel.log.dir:
-
 	 */
-
+	@PostConstruct
 	private static void initFlowQpsRule() {
 		//定义规则 可配置多个规则
 		List<FlowRule> rules = new ArrayList<FlowRule>();
@@ -35,11 +35,13 @@ public class SentinelConfig {
 		//4. 受来源限制的应用程序名称 默认 default
 		rule1.setLimitApp("default");
 		//5.添加到list
- 		rules.add(rule1);
+		rules.add(rule1);
 
 
- 		//6.加载规则
+		//6.加载规则
 		FlowRuleManager.loadRules(rules);
+		System.out.println("初始化限流策略成功");
 	}
+
 
 }
--- src/main/java/com/linjingc/top/sentineldemo/controller/HelloController.java
+++ src/main/java/com/linjingc/top/sentineldemo/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.linjingc.top.sentineldemo.controller;
 
 import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphO;
 import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.Tracer;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,20 +12,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("hello")
 public class HelloController {
 
+	/**
+	 * 异常限流处理  entry=SphU.entry("HelloWorld");
+	 * entry.exit();
+	 * 必须成对出现
+	 *
+	 * @return
+	 */
 	@RequestMapping("index")
 	public String index() {
 		Entry entry = null;
 		try {
 			entry = SphU.entry("HelloWorld");
-			System.out.println("HelloController.index 正常访问");
+			return "HelloController.index 正常访问";
 		} catch (BlockException e) {
-			System.out.println("HelloController.index 限流中");
 			e.printStackTrace();
+			return "HelloController.index 限流中";
 		} finally {
 			if (entry != null) {
 				entry.exit();
 			}
 		}
-		return "HelloWorld";
+	}
+
+	/**
+	 * if限流处理  SphO.entry("HelloWorld");
+	 *
+	 * @return
+	 */
+	@RequestMapping("index1")
+	public String index1() {
+		if (SphO.entry("HelloWorld")) {
+			return "HelloController.index1 正常访问";
+		} else {
+			return "HelloController.index1 限流中";
+		}
 	}
 }
